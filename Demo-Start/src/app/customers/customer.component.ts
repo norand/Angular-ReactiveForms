0ed1f20672f69e44ee481bc5c66b1ee2bbf08dd6
Vasyl Nikonenko <nvvuad@gmail.com>
2018-11-23 22:26:10 +0100

    Module 4 finished

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { FormGroup, FormBuilder } from  '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
@@ -9,15 +8,31 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      sendCatalog: true
+    });
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: "Vasyl",
+      lastName: "Nikonenko",
+      //email: "nvvuad@gmail.com",
+      sendCatalog: false
+    });
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
 }
